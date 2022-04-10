610a67db7c3c2bb3cb2dc48a2005d1e2e07c6cc5
dogukankaynar <dogukankaynar98@gmail.com>
2022-04-10 15:21:05 +0300

    first Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const sorular =[
+    {
+        soru1:'Türkiyenin Başkenti',
+        a:'Ankara',
+        b:'Bursa',
+        c:'İstdanbul',
+        d:'Mersin',
+        dogruCevap:'Ankara'
+    },
+    {
+        soru2:'Türkiyenin Başkenti',
+        a:'Ankara',
+        b:'Bursa',
+        c:'İstdanbul',
+        d:'Mersin',
+        dogruCevap:'Ankara'
+    }
+]
+
+
+sorular.forEach(getir=>console.log(getir))

377c9b66d42b091fd0006e68e914fcdba2de3467
dogukankaynar <dogukankaynar98@gmail.com>
2022-05-07 12:24:38 +0300

    remove p folder

--- js/app.js
+++ js/app.js
@@ -1,52 +1,44 @@
 const gonder = document.getElementById("gonder");
 const startQuiz = document.getElementById("startQuiz");
-const answers=document.querySelector('.answers');
-const nextQuestion=document.getElementById('gönder');
+const answers = document.querySelector(".answers");
+const nextQuestion = document.getElementById("gönder");
 const sinav = new Question();
 const print = new Print();
 let sayac = -1;
-let score=0;
-let yanliscevap=0;
-
+let score = 0;
+let yanliscevap = 0;
 
 startQuiz.addEventListener("click", removebutton);
 function removebutton() {
   startQuiz.parentElement.remove();
   //createQuiz();
   gonderSoru();
-  answers.addEventListener('click',check);
-  
+  answers.addEventListener("click", check);
 }
 
-
-
-
 function check(e) {
-    console.log(e.target.lastChild.textContent)
-    if(e.target.lastChild.textContent==sinav.sorular[sayac].dogruCevap){
-        score++;
-        console.log('scorunuz '+score)
-        console.log('cevap Dogru')
-        gonderSoru()
-    }
-    else{
-        yanliscevap++;
-        console.log('Yanlis cevap '+yanliscevap )
-        gonderSoru()
-    }
+  console.log(e.target.lastChild.textContent);
+  if (e.target.lastChild.textContent == sinav.sorular[sayac].dogruCevap) {
+    score++;
+    console.log("scorunuz " + score);
+    console.log("cevap Dogru");
+    gonderSoru();
+  } else {
+    yanliscevap++;
+    console.log("Yanlis cevap " + yanliscevap);
+    gonderSoru();
+  }
 }
 
 //  nextQuestion.addEventListener('click',gonderSoru)
 function gonderSoru() {
   sayac++;
-  if(sayac<sinav.sorular.length){
+  if (sayac < sinav.sorular.length) {
     print.printQuestion(sayac);
-   
-  }
-  else {
-      console.log('sinav bitti')
-      answers.remove();
-      main.remove();
-      print.endQuiz(score,yanliscevap);
+  } else {
+    console.log("sinav bitti");
+    answers.remove();
+    main.remove();
+    print.endQuiz(score, yanliscevap);
   }
 }
--- js/question.js
+++ js/question.js
@@ -1,37 +1,35 @@
-class Question{
-
-}
-Question.prototype.sorular=[
-    {
-        soru:'Türkiyenin Başkenti Neresidir',
-        a:'Ankara',
-        b:'Bursa',
-        c:'İstanbul',
-        d:'Edirne',
-        dogruCevap:'Ankara'
-    },
-    {
-        soru:'Cumhuriyet Kaç Yılında İlan Edilmiştir? ',
-        a:'1 Nisan',
-        b:'3 Mayıs',
-        c:'29 Ekim',
-        d:'30 Ağustos',
-        dogruCevap:'29 Ekim'
-    },
-    {
-        soru:'Ruh Adam Eseri Kime Aittir?',
-        a:'Namık Kemal',
-        b:'Nihal Atsız',
-        c:'Ziya Gökalp',
-        d:'Yusuf Akçura',
-        dogruCevap:'Nihal Atsız'
-    },
-    {
-        soru:'Kıbrıs Barış Harekatı Kaç Yılında Gerçekleşti?',
-        a:'1973',
-        b:'1971',
-        c:'1970',
-        d:'1974',
-        dogruCevap:'1974'
-    }
-]
+class Question {}
+Question.prototype.sorular = [
+  {
+    soru: "Türkiyenin Başkenti Neresidir",
+    a: "Ankara",
+    b: "Bursa",
+    c: "İstanbul",
+    d: "Edirne",
+    dogruCevap: "Ankara",
+  },
+  {
+    soru: "Cumhuriyet Kaç Yılında İlan Edilmiştir? ",
+    a: "1 Nisan",
+    b: "3 Mayıs",
+    c: "29 Ekim",
+    d: "30 Ağustos",
+    dogruCevap: "29 Ekim",
+  },
+  {
+    soru: "Ruh Adam Eseri Kime Aittir?",
+    a: "Namık Kemal",
+    b: "Nihal Atsız",
+    c: "Ziya Gökalp",
+    d: "Yusuf Akçura",
+    dogruCevap: "Nihal Atsız",
+  },
+  {
+    soru: "Kıbrıs Barış Harekatı Kaç Yılında Gerçekleşti?",
+    a: "1973",
+    b: "1971",
+    c: "1970",
+    d: "1974",
+    dogruCevap: "1974",
+  },
+];
